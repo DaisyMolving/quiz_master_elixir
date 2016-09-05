d254b4f4c921917414fbb17cb7b34223a14377b0
Daisy Molving <daisymolving@gmail.com>
2016-09-05 14:59:41 +0100

    changed name of main module in escript to be correct, reformatted questions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TriviaApp.Mixfile do
     [app: :trivia_app,
      version: "0.1.0",
      elixir: "~> 1.3",
-     escript: [main_module: QuizMaster],
+     escript: [main_module: TriviaApp],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
